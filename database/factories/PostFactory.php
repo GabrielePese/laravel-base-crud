4698a9873f711ee28330ac45c9c577efa269fd11
Gabriele <gabrielepese@gmail.com>
2020-09-29 16:49:34 +0200

    done

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Post;
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' =>$faker -> word(),
+        'text' => $faker -> text($maxNbChars = 200),
+        'category'=> $faker -> word(),
+        'like' => $faker -> randomDigit(),
+        'dislike' => $faker -> randomDigit()
+    ];
+});
